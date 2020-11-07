f2f64db4d82215efbb4c200a07480cb27d9d9cdd
stanislav7766 <stanislavtihonov5@gmail.com>
2020-11-07 03:24:58 +0200

    addeed check isTablet for default indent

--- src/constants/indent.ts
+++ src/constants/indent.ts
@@ -1,3 +1,4 @@
 import {widthPercentageToDP as wp} from '@utils/percetagetoDP';
+import {isTablet} from '@utils/isTablet';
 
-export const DEFAULT_INDENT: number = wp(10);
+export const DEFAULT_INDENT: number = wp(isTablet() ? 10 : 5);
