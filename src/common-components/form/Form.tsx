8a8bb38f3ebdcc0b26982d7dd1df1641da9871dc
stanislav7766 <stanislavtihonov5@gmail.com>
2020-11-23 03:13:14 +0200

    updated Form

--- src/common-components/form/Form.tsx
+++ src/common-components/form/Form.tsx
@@ -7,14 +7,17 @@ interface FormProps {
   children: ReactNode;
   backgroundColor?: string;
   height?: number;
+  width?: number;
 }
 
-const Form = ({children, backgroundColor, height}: FormProps) => {
+const Form = ({children, backgroundColor, height, width}: FormProps) => {
   const isHeight = height !== undefined;
-  return <View style={[styles.form, {backgroundColor}, isHeight && {height}]}>{children}</View>;
+  const isWidth = width !== undefined;
+  return <View style={[styles.form, {backgroundColor}, isHeight && {height}, isWidth && {width}]}>{children}</View>;
 };
 Form.defaultProps = {
   backgroundColor: ACCENT_COLOR_BLUE,
   height: undefined,
+  width: undefined,
 };
 export default Form;
