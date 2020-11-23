9b53b80335904137e95d418243d630667ad99b91
stanislav7766 <stanislavtihonov5@gmail.com>
2020-11-23 03:27:53 +0200

    updated runParalel params( adeed cb)

--- src/utils/animations/index.ts
+++ src/utils/animations/index.ts
@@ -1,5 +1,7 @@
 import {Animated} from 'react-native';
 
-export const runParalel = (animations: Array<Animated.CompositeAnimation>): void => {
-  Animated.parallel(animations).start();
+export const runParalel = (animations: Array<Animated.CompositeAnimation>, cb?: () => void): void => {
+  Animated.parallel(animations).start(() => {
+    cb && cb();
+  });
 };
