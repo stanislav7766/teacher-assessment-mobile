4799d2220840faafe37d7e53e14e392ffb862399
stanislav7766 <stanislavtihonov5@gmail.com>
2020-11-29 03:31:46 +0200

    changed Touchable

--- src/common-components/touchable/Touchable.tsx
+++ src/common-components/touchable/Touchable.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 import {IOnPress} from 'types/common';
 import {styles} from './styles';
 
 declare interface ITouchable {
   onPress: IOnPress;
   Child: JSX.Element;
+  withFeedback?: boolean;
 }
 
-const Touchable = ({Child, onPress}: ITouchable) => (
-  <TouchableOpacity style={styles.touchable} onPress={onPress}>
-    {Child}
-  </TouchableOpacity>
-);
+const Touchable = ({Child, withFeedback, onPress}: ITouchable) =>
+  withFeedback ? (
+    <TouchableOpacity style={styles.touchable} onPress={onPress}>
+      {Child}
+    </TouchableOpacity>
+  ) : (
+    <TouchableWithoutFeedback style={styles.touchable} onPress={onPress}>
+      <View>{Child}</View>
+    </TouchableWithoutFeedback>
+  );
+
+Touchable.defaultProps = {
+  withFeedback: true,
+};
 
 export default Touchable;
