2371339dd82de1dd6cc60c5fc59f379f6b9f39a1
stanislav7766 <stanislavtihonov5@gmail.com>
2020-11-29 23:23:16 +0200

    added animations util

--- src/utils/animations/index.ts
+++ src/utils/animations/index.ts
@@ -5,3 +5,15 @@ export const runParalel = (animations: Array<Animated.CompositeAnimation>, cb?: 
     cb && cb();
   });
 };
+
+export const runSequence = (animations: Array<Animated.CompositeAnimation>, cb?: () => void): void => {
+  Animated.sequence(animations).start(() => {
+    cb && cb();
+  });
+};
+
+export const getParalel = (animations: Array<Animated.CompositeAnimation>): Animated.CompositeAnimation =>
+  Animated.parallel(animations);
+
+export const getSequance = (animations: Array<Animated.CompositeAnimation>): Animated.CompositeAnimation =>
+  Animated.sequence(animations);
