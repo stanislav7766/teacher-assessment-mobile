64d0af772ffce8b3a69e3c85645d8467c6344a19
stanislav7766 <stanislavtihonov5@gmail.com>
2020-12-08 01:07:24 +0200

    updated validation

--- src/utils/validation/isEmpty.ts
+++ src/utils/validation/isEmpty.ts
@@ -1,2 +1,4 @@
 export const isEmptyString = (value: string | undefined): boolean =>
   !!(value !== undefined && value.trim().length === 0);
+
+export const isEmptyArray = (arr: Array<any>): boolean => arr.length === 0;
